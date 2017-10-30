e9ad1655c4e4b566e4d70b32ddb639f95042f745
Louis-He <1726110778@qq.com>
2017-10-30 15:29:25 -0400

    Update lab7.m

--- lab7.m
+++ lab7.m
@@ -1,7 +1,8 @@
 E = 68.9e9 ; 
 %Aluminum Alloy 1100: 68.9e9 GPa
 %Nickel 200: 200e9 GPa
-%Steel Alloy 4340: 200e9 GPs
+%Steel Alloy 4340: 200e9 GPa
+p1 = 50;%plot choice, the weight of P
 
 AD = true;
 BE = true;
@@ -22,7 +23,7 @@ for p = 1:50
     j = 0;
     for d_01 = 1:160
         j = j + 1;
-        B = [p * 1000;d_01/10.0*p * 1000;0];
+        B = [p * 1000;d_01 / 10.0 * p * 1000;0];
         solution = A\B;
         Fad(i,j) = solution(1,1);
         Fbe(i,j) = solution(2,1);
@@ -36,28 +37,28 @@ for p = 1:50
         dLbep(i,j) = dLbe / 5.0;
         dLcgp(i,j) = dLcg / 2.0;
         
-        if dLad / 4 > 0.1
+        if dLad / 4 > 0.001
            AD = false;
         end
-        if dLbe / 5 > 0.1
+        if dLbe / 5 > 0.001
            BE = false;
         end
-        if dLcg / 2 > 0.1
+        if dLcg / 2 > 0.001
            CG = false;
         end
     end
 end
 
 x = linspace(0,16,160);
-plot(x, dLadp(50,:))
+plot(x, dLadp(p1,:))
 hold on
-plot(x, dLbep(50,:))
-plot(x, dLcgp(50,:))
+plot(x, dLbep(p1,:))
+plot(x, dLcgp(p1,:))
 grid on
 legend('Fad','Fbe','Fcg')
 
 if AD && BE && CG
    true
 else
-    false
+   false
 end
