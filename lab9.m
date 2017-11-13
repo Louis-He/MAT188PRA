c4d8daf15e0b6468777fd7663bee33350713d96e
Louis-He <1726110778@qq.com>
2017-11-13 15:26:31 -0500

    Update lab9.m

--- lab9.m
+++ lab9.m
@@ -3,25 +3,25 @@ y = [-2:0.1:2];
 [X, Y]=meshgrid(x, y);
 G = X.*exp(-X.*X-Y.*Y);
 surfc(X,Y,G);
-title("plot of x*e^(-x^2-y^2)");
+title('plot of x*e^(-x^2-y^2)');
 xlabel('x');ylabel('y');zlabel('z');
 
 y1 = -2;
 G1 = x.*exp(-x.*x-y1.*y1);
 plot(x,G1);
-title("plot of x*e^(-x^2-y^2), y = -2");
+title('plot of x*e^(-x^2-y^2), y = -2');
 xlabel('x');ylabel('z');
 
 x1 = -1;
 G1 = x1.*exp(-x1.*x1-y.*y);
 plot(y,G1);
-title("plot of x*e^(-x^2-y^2), x = -1");
+title('plot of x*e^(-x^2-y^2), x = -1');
 xlabel('y');ylabel('z');
 
 y2 = 0;
 G1 = x.*exp(-x.*x-y2.*y2);
 plot(x,G1);
-title("plot of x*e^(-x^2-y^2), y = 0");
+title('plot of x*e^(-x^2-y^2), y = 0');
 xlabel('x');ylabel('z');
 
 [maxx, maxpos] = max(G);
